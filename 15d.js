78b3206e1d1a0771a510b9bda6af4ad4c17473b7
Jakob Sertcanli <149320921+JakobSertcanli@users.noreply.github.com>
2025-05-06 20:03:40 +0200

    Delete 15d.js

--- 15d.js
+++
@@ -1,9 +0,0 @@
-import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import isSatsun from './15e.js';
-
-
- const date = dayjs();
- console.log(isSatsun(date));
- console.log(isSatsun(date));
- console.log(isSatsun(date));
-
