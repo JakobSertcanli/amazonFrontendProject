e45a450580aa81d317e166aa438269b59aa21784
Jakob Sertcanli <149320921+JakobSertcanli@users.noreply.github.com>
2025-05-06 20:03:30 +0200

    Delete 15c.js

--- 15c.js
+++
@@ -1,8 +0,0 @@
-import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-
-
-const today = dayjs();
-const before = today.subtract(1, 'month');
-const format = before.format('MMMM D');
-console.log(format);
-
